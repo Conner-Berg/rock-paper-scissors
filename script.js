86c46683fbe03ce8b7947917a77dd6e2439a0d9f
Conner-Berg <connerwberg@gmail.com>
2023-01-09 14:57:30 -0500

    create script.js with assignment steps 3-5

--- script.js
+++ script.js
@@ -4,31 +4,27 @@ let result;
 
 function startRockPaperScissors() {
 	if (playerSelection === undefined) {
-		let gamePrompt =
-			'Hi, lets play Rock-Paper-Scissors!\nTo start playing, type "rock", "paper", "scissors".\nTo play a round of 5 games, type "5".\nTo learn how to play, type "?".';
-	} else if (playerSelection === 5) {
-		let gamePrompt = `Time for round ${gameNum}!\nType "rock", "paper", "scissors".`;
+		playerSelection = prompt(
+			'Hi, lets play Rock-Paper-Scissors!\nTo start playing, type "rock", "paper", "scissors".'
+		);
 	} else if (
 		playerSelection === "rock" ||
 		playerSelection === "paper" ||
 		playerSelection === "scissors"
 	) {
-		let gamePrompt = `${result}\nTo play again, type "rock", "paper", "scissors".\nTo play a round of 5 games, type "5".`;
+		playerSelection = prompt(
+			`${result}\nTo play again, type "rock", "paper", "scissors".`
+		);
 	}
-	getInput();
+	setTimeout(showPlayerSelection, 1); // Chrome wouldn't console.log between prompts without setTimeout
 }
 
-function getInput() {
-	playerSelection = prompt(gamePrompt);
-	showInput();
-}
-
-function showInput() {
+function showPlayerSelection() {
 	console.log(`You typed: ${playerSelection}`);
-	checkInput();
+	checkPlayerSelection();
 }
 
-function checkInput() {
+function checkPlayerSelection() {
 	if (playerSelection === "" || playerSelection === null) {
 		alert(
 			"Oops, it looks like you didn't enter anything.\nPress OK, or refresh the page to start over."
@@ -40,12 +36,9 @@ function checkInput() {
 		playerSelection.toLowerCase() === "paper" ||
 		playerSelection.toLowerCase() === "scissors"
 	) {
+		getComputerSelection();
 		playRound();
 		startRockPaperScissors();
-	} else if (playerSelection === 5) {
-		game();
-	} else if (playerSelection === "?") {
-		showRules();
 	} else {
 		alert(
 			"Hmm, I didn't understand what you typed.\nPress OK, or refresh the page to start over."
@@ -67,7 +60,6 @@ function getComputerSelection() {
 }
 
 function playRound() {
-	getComputerSelection();
 	if (playerSelection === computerSelection) {
 		result = "It's a tie!";
 	} else if (
@@ -82,27 +74,6 @@ function playRound() {
 	result = `The computer picked ${computerSelection}. ${result}`;
 	console.log(result);
 	return result;
-}
-
-function game() {
-	let playerGamesWon = 0;
-	let computerGamesWon = 0;
-	let gamesTied = 0;
-	for (let gameNum = 1; gameNum <= 5; gameNum++) {
-		startRockPaperScissors();
-		playRound();
-		if (result === "It's a tie!") {
-			gamesTied += 1;
-		} else if (result === "You win!") {
-			playerGamesWon += 1;
-		} else if (result === "You lose!") {
-			computerGamesWon += 1;
-		}
-	}
-}
-
-function showRules() {
-	// In progress
 }
 
 startRockPaperScissors();
